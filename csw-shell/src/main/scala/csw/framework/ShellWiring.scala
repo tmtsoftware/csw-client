3e84e26748e06bfd5d2729056e06a4c354585713
Shubham Jaybhaye <shubhamj7997@gmail.com>
2020-05-19 14:53:10 +0530

    Shubham : Fix the compilation broken by version upgrade

--- csw-shell/src/main/scala/csw/framework/ShellWiring.scala
+++ csw-shell/src/main/scala/csw/framework/ShellWiring.scala
@@ -10,9 +10,8 @@ import shell.utils.Extensions.FutureExt
 class ShellWiring {
   lazy val wiring = new FrameworkWiring
   import wiring._
-  import actorRuntime._
 
-  private implicit lazy val cswFrameworkSystem: CswFrameworkSystem = new CswFrameworkSystem(typedSystem)
+  private implicit lazy val cswFrameworkSystem: CswFrameworkSystem = new CswFrameworkSystem(actorSystem)
 
   lazy val cswContext: CswContext =
     CswContext
--- csw-shell/src/main/scala/esw/CommandServiceDsl.scala
+++ csw-shell/src/main/scala/esw/CommandServiceDsl.scala
@@ -7,11 +7,16 @@ import csw.command.client.CommandServiceFactory
 import csw.command.client.extensions.AkkaLocationExt.RichAkkaLocation
 import csw.framework.ShellWiring
 import csw.location.api.models.ComponentType.{Assembly, HCD}
+import csw.location.api.models.Connection.AkkaConnection
+import csw.location.api.models.{AkkaLocation, ComponentId, ComponentType}
 import csw.prefix.models.{Prefix, Subsystem}
-import esw.ocs.impl.SequencerActorProxy
-import esw.ocs.impl.internal.LocationServiceUtil
+import esw.commons.utils.location.{EswLocationError, LocationServiceUtil}
+import esw.ocs.api.SequencerApi
+import esw.ocs.api.actor.client.SequencerImpl
 import shell.utils.Extensions.FutureExt
 import shell.utils.Timeouts
+
+import scala.concurrent.Future
 
 class CommandServiceDsl(val shellWiring: ShellWiring) {
   implicit lazy val typedSystem: ActorSystem[SpawnProtocol.Command] = shellWiring.wiring.actorSystem
@@ -21,15 +26,25 @@ class CommandServiceDsl(val shellWiring: ShellWiring) {
   private implicit val implicitTimeout: Timeout = Timeouts.defaultTimeout
   private val locationUtil: LocationServiceUtil = new LocationServiceUtil(shellWiring.cswContext.locationService)
 
-  def sequencerCommandService(subsystem: Subsystem, observingMode: String): SequencerActorProxy =
+  def sequencerCommandService(subsystem: Subsystem, observingMode: String): SequencerApi =
     locationUtil
       .resolveSequencer(subsystem, observingMode)
-      .map(akkaLocation => new SequencerActorProxy(akkaLocation.sequencerRef))
+      .map(e => new SequencerImpl(throwLeft(e).sequencerRef))
       .await()
 
   def assemblyCommandService(prefix: String): CommandService =
-    CommandServiceFactory.make(locationUtil.resolveAkkaLocation(Prefix(prefix), Assembly).await())
+    CommandServiceFactory.make(resolveAkkaLocation(prefix, Assembly).await())
 
   def hcdCommandService(prefix: String): CommandService =
-    CommandServiceFactory.make(locationUtil.resolveAkkaLocation(Prefix(prefix), HCD).await())
+    CommandServiceFactory.make(resolveAkkaLocation(prefix, HCD).await())
+
+  def resolveAkkaLocation(prefix: String, componentType: ComponentType): Future[AkkaLocation] =
+    locationUtil
+      .resolve(AkkaConnection(ComponentId(Prefix(prefix), HCD)))
+      .map(throwLeft)
+
+  def throwLeft[T](e: Either[EswLocationError, T]): T = e match {
+    case Right(t)  => t
+    case Left(err) => throw new RuntimeException(err.msg)
+  }
 }
